60f3d826fa1f6f72fdac2b6ee92924b472f96f1e
julio.hidalgo.cascante <46944166+jhidalgocascante@users.noreply.github.com>
2022-04-25 20:55:23 -0600

    Se agrega ultimo commit para entregar proyecto

--- HealthPet/Controllers/CitaController.cs
+++ HealthPet/Controllers/CitaController.cs
@@ -2,7 +2,14 @@
 using HealthPet.Models;
 using HealthPet.Datos;
 using Rotativa.AspNetCore;
-
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.tool.xml;
+using iTextSharp.text.html.simpleparser;
+using System;
+using System.Web;
+using System.Data;
 
 namespace HealthPet.Controllers
 {
@@ -99,11 +106,62 @@ namespace HealthPet.Controllers
             //Devolver solo la vista formato hmtml
             var oCita = _CitaDatos.Obtener(CodCita);
             //return View(oAvion);
-
-            return new ViewAsPdf("Generar", oCita);//Nombre de la vista
+            return View(oCita);
+            //return new ViewAsPdf("Generar", oCita);//Nombre de la vista
             //return new ActionAsPdf("Generar", oCita);
         }
 
+
+        //recibe el objeto que se ha modificado para ajustarlo con el objeto
+        [HttpPost]
+        public IActionResult Generar(CitaModel oCita)
+        {
+            //Valido si existe algun tipo de validacion que revisar y pendiente
+            if (!ModelState.IsValid)
+                return View();
+
+            //metodo recibe el objeto para guardarlo en bd
+            var respuesta = _CitaDatos.Eliminar(oCita.CodCita);
+
+            if (respuesta)
+                return RedirectToAction("Listar");
+            else
+                return View();
+        }
+
+
+        public IActionResult Export(int CodCita)
+        {
+
+            //Devolver solo la vista formato hmtml
+            var oCita = _CitaDatos.Obtener(CodCita);
+            //return View(oAvion);
+            return View(oCita);
+            //return new ViewAsPdf("Generar", oCita);//Nombre de la vista
+            //return new ActionAsPdf("Generar", oCita);
+        }
+
+        //[ValidateInput(false)]
+        [HttpPost]
+        public FileResult Export(string GridHtml)
+        {
+            using (MemoryStream stream = new System.IO.MemoryStream())
+            {
+
+                return File(System.Text.Encoding.ASCII.GetBytes(GridHtml), "application/vnd.ms-excel", "Grid.xls");
+    
+
+
+
+        /*StringReader sr = new StringReader(GridHtml);
+        Document pdfDoc = new Document(PageSize.A4, 10f, 10f, 100f, 0f);
+        PdfWriter writer = PdfWriter.GetInstance(pdfDoc, stream);
+        pdfDoc.Open();
+        XMLWorkerHelper.GetInstance().ParseXHtml(writer, pdfDoc, sr);
+        pdfDoc.Close();
+        return File(stream.ToArray(), "application/pdf", "Grid.pdf");*/
+    }
+}
 
     }
 }
